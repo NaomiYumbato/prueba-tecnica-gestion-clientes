3641974fb8644a2b317ed9ca3b8854b8751aec19
Naomi <yumbatolopeznaomi@gmail.com>
2026-01-14 15:44:59 -0500

    modelado JPA de Cliente y TipoCliente

---
+++ src/main/java/com/prueba/tecnica/gestionclientes/entity/ClienteEntity.java
@@ -0,0 +1,33 @@
+package com.prueba.tecnica.gestionclientes.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "cliente")
+public class ClienteEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "nombre_completo", nullable = false, length = 50)
+    private String nombreCompleto;
+    @Column(name = "nro_documento", length = 8, nullable = false, unique = true)
+    private String nroDocumento;
+    @Column(nullable = false, length = 100)
+    private String email;
+    @Column(nullable = false, length = 15)
+    private String celular;
+    @ManyToOne
+    @JoinColumn(name = "id_tipo", nullable = false)
+    private TipoClienteEntity tipoCliente;
+}
