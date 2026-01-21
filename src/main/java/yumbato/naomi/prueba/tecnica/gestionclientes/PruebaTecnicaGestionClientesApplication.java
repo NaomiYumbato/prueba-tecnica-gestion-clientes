243179c0635933df8b9389fa8604722a29e66a29
Naomi <naomi.yumbato.ext@entelgy.com>
2026-01-21 16:11:41 -0500

    Proyecto base

---
+++ src/main/java/yumbato/naomi/prueba/tecnica/gestionclientes/PruebaTecnicaGestionClientesApplication.java
@@ -0,0 +1,13 @@
+package yumbato.naomi.prueba.tecnica.gestionclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaGestionClientesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaTecnicaGestionClientesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yumbato/naomi/prueba/tecnica/gestionclientes/PruebaTecnicaGestionClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package yumbato.naomi.prueba.tecnica.gestionclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaGestionClientesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
